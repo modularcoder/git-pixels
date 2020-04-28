c4c9e91855157edb7dd644c959db092d4e7062c9
Gevorg Harutyunyan <gevharut@gmail.com>
2020-04-28 07:31:53 +0400

    Updates

--- src/_common/AppIntro/AppIntro.vue
+++ src/_common/AppIntro/AppIntro.vue
@@ -3,7 +3,8 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
-          Git-Pixels by @modularcoder
+          Git-Pixels by
+          <a href="https://github.com/modularcoder">@modularcoder</a>
         </h1>
         <h2 class="subtitle">
           A simple tool for creating GitHub contributions charts
